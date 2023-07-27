537057223525d5b56553be134c33a449fb672e1a
Andrey Carvajal <ancarsa.wasabi.12@gmail.com>
2023-07-27 11:49:13 -0500

    add update

--- src/App.js
+++ src/App.js
@@ -16,10 +16,12 @@ import { listNotes } from "./graphql/queries";
 import {
   createNote as createNoteMutation,
   deleteNote as deleteNoteMutation,
+  updateNote as updateNoteMutation,
 } from "./graphql/mutations";
 
 const App = ({ signOut }) => {
   const [notes, setNotes] = useState([]);
+  const [updateNoteId, setUpdateNoteId] = useState(null);
 
   useEffect(() => {
     fetchNotes();
@@ -39,10 +41,7 @@ const App = ({ signOut }) => {
     );
     setNotes(notesFromAPI);
   }
-//The error you are encountering, "Cannot read properties of null (reading 'name')",
-// is likely happening when trying to access the name property of the image input element in the 
-//form. This error occurs when there is no file selected for the image input, and thus, 
-//the image element is null.
+
   async function createNote(event) {
     event.preventDefault();
     const form = new FormData(event.target);
@@ -50,7 +49,7 @@ const App = ({ signOut }) => {
     const data = {
       name: form.get("name"),
       description: form.get("description"),
-      image: image ? image.name : null, // Check if image is selected, otherwise set it to null
+      image: image ? image.name : null,
     };
     if (data.image) await Storage.put(data.name, image);
     await API.graphql({
@@ -60,8 +59,6 @@ const App = ({ signOut }) => {
     fetchNotes();
     event.target.reset();
   }
-  
-  
 
   async function deleteNote({ id, name }) {
     const newNotes = notes.filter((note) => note.id !== id);
@@ -73,10 +70,33 @@ const App = ({ signOut }) => {
     });
   }
 
+  async function updateNote(event) {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    const image = form.get("image");
+    const data = {
+      name: form.get("name"),
+      description: form.get("description"),
+      image: image ? image.name : null,
+    };
+    if (data.image) await Storage.put(data.name, image);
+    await API.graphql({
+      query: updateNoteMutation,
+      variables: { input: { ...data, id: updateNoteId } },
+    });
+    setUpdateNoteId(null);
+    fetchNotes();
+    event.target.reset();
+  }
+
+  function editNote(note) {
+    setUpdateNoteId(note.id);
+  }
+
   return (
-    <View className="App">
+    <View className="App" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
       <Heading level={1}>My Notes App</Heading>
-      <View as="form" margin="3rem 0" onSubmit={createNote}>
+      <View as="form" margin="3rem 0" onSubmit={updateNote} enctype="multipart/form-data">
         <Flex direction="row" justifyContent="center">
           <TextField
             name="name"
@@ -94,46 +114,58 @@ const App = ({ signOut }) => {
             variation="quiet"
             required
           />
+          <View
+            name="image"
+            as="input"
+            type="file"
+            style={{ alignSelf: "end" }}
+          />
           <Button type="submit" variation="primary">
-            Create Note
+            {updateNoteId ? "Update Note" : "Create Note"}
           </Button>
         </Flex>
       </View>
-      <View
-  name="image"
-  as="input"
-  type="file"
-  style={{ alignSelf: "end" }}
-/>
       <Heading level={2}>Current Notes</Heading>
-      <View margin="3rem 0">
-      {notes.map((note) => (
-  <Flex
-    key={note.id || note.name}
-    direction="row"
-    justifyContent="center"
-    alignItems="center"
-  >
-    <Text as="strong" fontWeight={700}>
-      {note.name}
-    </Text>
-    <Text as="span">{note.description}</Text>
-    {note.image && (
-      <Image
-        src={note.image}
-        alt={`visual aid for ${notes.name}`}
-        style={{ width: 400 }}
-      />
-    )}
-    <Button variation="link" onClick={() => deleteNote(note)}>
-      Delete note
-    </Button>
-  </Flex>
-))}
+      <View margin="3rem 0" style={{ width: "80%", maxWidth: "800px" }}>
+        <table style={{ width: "100%" }}>
+          <thead>
+            <tr>
+              <th>Note Name</th>
+              <th>Note Description</th>
+              <th>Image</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {notes.map((note) => (
+              <tr key={note.id || note.name}>
+                <td>{note.name}</td>
+                <td>{note.description}</td>
+                <td>
+                  {note.image && (
+                    <Image src={note.image} alt={`visual aid for ${note.name}`} style={{ width: 100 }} />
+                  )}
+                </td>
+                <td>
+                  <Button variation="link" onClick={() => editNote(note)}>
+                    Edit
+                  </Button>
+                  <Button variation="link" onClick={() => deleteNote(note)}>
+                    Delete
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </View>
+      {updateNoteId && (
+        <Button onClick={() => setUpdateNoteId(null)}>Cancel Edit</Button>
+      )}
       <Button onClick={signOut}>Sign Out</Button>
     </View>
   );
 };
 
 export default withAuthenticator(App);
+
